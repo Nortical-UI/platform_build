6b0a3f5dcad5606680d2b659d62b0c0d03d6331a
MoetaYuko <loli@yuko.moe>
2022-01-22 01:16:41 +0800

    core: Include nortical vendor configs

--- core/config.mk
+++ core/config.mk
@@ -1173,4 +1173,7 @@ DEFAULT_DATA_OUT_MODULES := ltp $(ltp_packages) $(kselftest_modules)
 # Make RECORD_ALL_DEPS readonly.
 RECORD_ALL_DEPS :=$= $(filter true,$(RECORD_ALL_DEPS))
 
+# Include Nortical configs.
+include vendor/nortical/build/core/config.mk
+
 include $(BUILD_SYSTEM)/dumpvar.mk
